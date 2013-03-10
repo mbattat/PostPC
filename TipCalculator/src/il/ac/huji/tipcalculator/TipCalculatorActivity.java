cf775e41fc6e9bd95b2adab732b012b4112ca21e
mbattat <red_fuzzball@hotmail.com>
2013-03-11 00:09:30 +0200

    second commit

--- TipCalculator/src/il/ac/huji/tipcalculator/TipCalculatorActivity.java
+++ TipCalculator/src/il/ac/huji/tipcalculator/TipCalculatorActivity.java
@@ -1,14 +1,19 @@
 package il.ac.huji.tipcalculator;
 
+import java.text.DecimalFormat;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.Checkable;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class TipCalculatorActivity extends Activity {
+
+	protected static final double TIP = 0.12;
+	protected static final String VIEW_FORMAT = "#.##";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +23,41 @@ public class TipCalculatorActivity extends Activity {
 		final EditText edtBillAmount = (EditText) findViewById(R.id.edtBillAmount);
 		final CheckBox chkRound = (CheckBox) findViewById(R.id.chkRound);
 		final Button btnCalculate = (Button) findViewById(R.id.btnCalculate);
-	}
-	
-	/** Called when the user clicks the Calculate button 
-	 *  Calculates tip of 12%.
-	 * */
-	public void calculateTip(View view) {
-	    
+		
+		btnCalculate.setOnClickListener(
+				new View.OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						
+						TextView txtTipResult = (TextView) findViewById(R.id.txtTipResult);
+						boolean roundTip = chkRound.isChecked();
+						String amount = edtBillAmount.getText().toString();
+						double amountToDouble, resault;
+						
+						try {
+							amountToDouble = Double.valueOf(amount);
+						}
+						catch (NumberFormatException nfe) {
+							//Value is not valid.
+							amountToDouble = -1;
+						}
+						
+						if (amountToDouble < 0) {
+							txtTipResult.setText(R.string.inputError);
+							return;
+						}
+						
+						resault = amountToDouble * TIP;
+						
+						if (roundTip) {
+							resault = (Math.floor(resault + 0.5));
+						}
+						
+						DecimalFormat df = new DecimalFormat(VIEW_FORMAT);
+						txtTipResult.setText(getString(R.string.dollar) + df.format(resault));
+					}
+				});
 	}
 
 }
